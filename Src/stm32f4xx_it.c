3e1081ab3d0dae7184de7e5ff60f56965df9c6be
dhrachov <dima.grach333@gmail.com>
2026-04-18 20:11:40 +0300

    Fix uart byte recieving

--- Src/stm32f4xx_it.c
+++ Src/stm32f4xx_it.c
@@ -205,12 +205,6 @@ void USART1_IRQHandler(void)
 {
   /* USER CODE BEGIN USART1_IRQn 0 */
 
-  if (__HAL_UART_GET_FLAG(&huart1, UART_FLAG_IDLE) != RESET)
-  {
-    __HAL_UART_CLEAR_IDLEFLAG(&huart1);
-    g_uartRxPending = true;
-  }
-
   /* USER CODE END USART1_IRQn 0 */
   HAL_UART_IRQHandler(&huart1);
   /* USER CODE BEGIN USART1_IRQn 1 */
